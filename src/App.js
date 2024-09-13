64115ac120a1f7b7f69b406edc29a7cda4024517
Pearltee28 <117785687+Pearltee28@users.noreply.github.com>
2024-09-13 02:38:41 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./styles.css";
+import SearchEngine from "./SearchEngine";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Weather app</h1>
+
+      <SearchEngine />
+    </div>
+  );
+}
