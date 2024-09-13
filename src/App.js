174a015d06c1d723c18b5c9b71f218add721700f
Pearltee28 <pearlngubane127@gmail.com>
2024-09-13 03:22:18 +0200

    updated the footer

--- src/App.js
+++ src/App.js
@@ -5,16 +5,19 @@ export default function App() {
   return (
     <div className="App">
       <h1>Weather app</h1>
+
+      <SearchEngine />
+
       <div className="footer">
-        This project was coded by
+        This project was coded by{" "}
         <a
           href="https://www.shecodes.io/graduates/118596-nokwazi-nomfundo-pearl-ngubane"
           target="_blank"
           rel="noreferrer"
         >
           Pearl Ngubane
-        </a>
-        and is
+        </a>{" "}
+        and is{" "}
         <a
           href="https://github.com/Pearltee28/react-weaather-app"
           target="_blank"
@@ -23,8 +26,6 @@ export default function App() {
           open-sourced
         </a>
       </div>
-
-      <SearchEngine />
     </div>
   );
 }
