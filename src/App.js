ffb0c8244d37247e5c0b495d5717ab9d0f3cdfad
Pearltee28 <pearlngubane127@gmail.com>
2024-09-13 03:13:40 +0200

    Added a footer

--- src/App.js
+++ src/App.js
@@ -5,6 +5,24 @@ export default function App() {
   return (
     <div className="App">
       <h1>Weather app</h1>
+      <div className="footer">
+        This project was coded by
+        <a
+          href="https://www.shecodes.io/graduates/118596-nokwazi-nomfundo-pearl-ngubane"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Pearl Ngubane
+        </a>
+        and is
+        <a
+          href="https://github.com/Pearltee28/react-weaather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced
+        </a>
+      </div>
 
       <SearchEngine />
     </div>
